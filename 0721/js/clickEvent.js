b5974498154e32d6d95a06f5b8cbd0c6e20b1aaf
Jee Hoon <yamoo9@naver.com>
2015-07-21 16:09:14 +0900

    on 클래스 라디오화

--- 0721/js/clickEvent.js
+++ 0721/js/clickEvent.js
@@ -38,3 +38,64 @@ function wallpaperInit() {
 	wallpaper.style.cssText = 'height: ' + viewportHeight + 'px; background: url(images/wallpaper-01.jpg) no-repeat center top; background-size: cover;';
 	return false;
 };
+
+
+/**
+ * --------------------------------
+ * .controller 메뉴 제어
+ * DOM Selector API - IE 8+
+ * .querySelector()
+ * .querySelectorAll()
+ * --------------------------------
+ */
+// IE 9+
+// var controller = document.getElementsByClassName('controller')[0];
+var controller = document.querySelector('.controller');
+// console.log(controller);
+
+// var controller_btns = controller.getElementsByTagName('a');
+// var controller_btns = controller.querySelectorAll('a');
+var controller_btns = document.querySelectorAll('.controller [role]');
+// console.log(controller_btns);
+
+controller_btns[0].onclick = function() {
+	// console.log(this.firstChild.nodeValue);
+	wallpaper.style.backgroundImage = 'url(images/wallpaper-01.jpg)';
+	var activeEl = controller.querySelector('.on');
+	activeEl.className = '';
+	this.className = 'on';
+	return false;
+};
+
+controller_btns[1].onclick = function() {
+	// console.log(this.firstChild.nodeValue);
+
+	// wallpaper의 배경이미지 값을 변경합니다.
+	wallpaper.style.backgroundImage = 'url(images/wallpaper-02.jpg)';
+
+	// 활성화(on)된 요소를 찾아서 활성화 클래스를 제거합니다.
+	var activeEl = controller.querySelector('.on');
+	activeEl.className = '';
+
+	// 클릭된 버튼(a요소)에 활성화 클래스를 설정합니다.
+	this.className = 'on';
+	return false;
+};
+
+controller_btns[2].onclick = function() {
+	// console.log(this.firstChild.nodeValue);
+	wallpaper.style.backgroundImage = 'url(images/wallpaper-03.jpg)';
+	var activeEl = controller.querySelector('.on');
+	activeEl.className = '';
+	this.className = 'on';
+	return false;
+};
+
+controller_btns[3].onclick = function() {
+	// console.log(this.firstChild.nodeValue);
+	wallpaper.style.backgroundImage = 'url(images/wallpaper-04.jpg)';
+	var activeEl = controller.querySelector('.on');
+	activeEl.className = '';
+	this.className = 'on';
+	return false;
+};
