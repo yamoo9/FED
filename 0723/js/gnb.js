b912d25361556a0079b4123812e160de6d9603ea
Jee Hoon <yamoo9@naver.com>
2015-07-23 16:13:45 +0900

    자바스크립트 반복문: while문

--- 0723/js/gnb.js
+++ 0723/js/gnb.js
@@ -1,3 +1,29 @@
-var gnb = $('#gnb');
+var gnb = $('#gnb'),
+	gnb_d1_links = $('#gnb .lv-1 > li > a');
 
-console.log(gnb);
+var count = 0;
+
+// while(count < 10) {
+	// console.log(count); // 0
+	// count = count + 1;
+	// count += 1;
+	// ++count; // 1
+// }
+
+/**
+ * --------------------------------
+ * 자바스크립트 반복문: while문
+ * --------------------------------
+ */
+
+var k = 0,
+	l = gnb_d1_links.length;
+
+while(k < l) {
+	gnb_d1_links[k].onfocus = gnbD1Action;
+	k++;
+}
+
+function gnbD1Action() {
+	console.log(this.textContent);
+};
