791fda7ade9d75a74a6453984c44503d55db186e
Jee Hoon <yamoo9@naver.com>
2015-07-23 16:27:00 +0900

    선/후 증가(감소) 사례에 따른 조건 처리 구문 확인

--- 0723/js/gnb.js
+++ 0723/js/gnb.js
@@ -16,14 +16,36 @@ var count = 0;
  * --------------------------------
  */
 
-var k = 0,
-	l = gnb_d1_links.length;
+// var k = 0,
+// 	l = gnb_d1_links.length;
 
-while(k < l) {
-	gnb_d1_links[k].onfocus = gnbD1Action;
-	k++;
-}
+// while(k < l) {
+// 	gnb_d1_links[k].onfocus = gnbD1Action;
+// 	k++;
+// }
 
 function gnbD1Action() {
 	console.log(this.textContent);
 };
+
+/**
+ * --------------------------------
+ * 선증가(감소) vs 후증가(감소)
+ * --------------------------------
+ */
+// --------------------------------/
+// 선 감소 사례
+// var l = gnb_d1_links.length;
+
+// while(--l >= 0) {
+// 	gnb_d1_links[l].onfocus = gnbD1Action;
+// }
+
+// --------------------------------/
+// 후 감소 사례
+// var l = gnb_d1_links.length;
+
+// while(l-- > 0) {
+// 	gnb_d1_links[l].onfocus = gnbD1Action;
+// }
+
