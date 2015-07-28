4991079ebfd864b7f6001ceedb2fab33f1e236dd
Jee Hoon <yamoo9@naver.com>
2015-07-28 13:41:08 +0900

    0728 진행 준비 및 오전 시간 내용 요약

---
+++ LECTURE_DATA/0728/widget_GNB/js/gnb.js
@@ -0,0 +1,88 @@
+/**
+ * ================================
+ * Javascript MVC 디자인 패턴
+ * --------------------------------
+ * Model      - 구조(Data)
+ * View       - 표현(Template)
+ * Controller - 동작(Behavior)
+ * --------------------------------
+ * 각 계층은 독립적으로 서로 의존하지 않고,
+ * 분리되도록 설계(Design)되어야 한다.
+ * ================================
+ */
+
+/**
+ * ================================
+ * [M]odel
+ * --------------------------------
+ * 구조화된 View 템플릿에 적용되는 Data
+ * HTML, XML, JSON ...
+ * AJAX - 비동기 통신
+ */
+
+
+/**
+ * ================================
+ * [V]iew
+ * --------------------------------
+ * 뷰 타겟(View Target) 설정
+ * CSS 선택자를 활용하여 문서 객체 참조
+ */
+var gnb                  = $('#gnb'), // Selecting
+	// gnb_expand_handle = find(gnb, '.expand-handle'), // Traversing
+	gnb_links            = $('a', gnb),
+	gnb_expand_handle    = $('.expand-handle', gnb);
+
+/**
+ * ================================
+ * [C]ontroller
+ * --------------------------------
+ * 뷰 타겟에 이벤트 연결
+ * <li> ⇒ MOUSE 이벤트 연결
+ * <a>  ⇒ KEYBOARD 이벤트 연결
+ *
+ * 이벤트 핸들러(함수) 정의
+ * expandMenu()
+ * collapseMenu()
+ */
+
+for( var i=0, l=gnb_expand_handle.length, handle, parent; i<l; i++ ) {
+
+	// gnb_expand_handle의 개별 아이템 참조 변수
+	handle = gnb_expand_handle[i];
+	parent = handle.parentNode;
+
+	// 각 핸들에 키보드(포커스) 이벤트 연결 → 함수(핸들러)
+	handle.onfocus = expandMenu;
+
+	// 핸들의 부모(<li>)에 마우스 이벤트 연결 → 함수(핸들러)
+	parent.onmouseenter = expandMenu;
+	parent.onmouseleave = collapseMenu;
+}
+
+/**
+ * --------------------------------
+ * 이벤트 핸들러(함수)
+ * --------------------------------
+ */
+
+// 메뉴 펼침 함수
+function expandMenu() {
+	var _this = this.nodeName.toLowerCase();
+	if(_this === 'a') {
+		radioClass(this.parentNode, 'on');
+	} else {
+		radioClass(this, 'on');
+	}
+}
+
+// 메뉴 접힘 함수
+function collapseMenu() {
+	var i=0, l=gnb_expand_handle.length, parent;
+	for(; i<l; i+=1) {
+		parent = gnb_expand_handle[i].parentNode;
+		if( hasClass(parent, 'on') ) {
+			removeClass(parent, 'on');
+		}
+	}
+}
--- LECTURE_DATA/Libs/dom-helpers.js
+++ LECTURE_DATA/Libs/dom-helpers.js
@@ -1,5 +1,28 @@
 /**
  * ======================================================================
+ * 생성(Creating)
+ * ======================================================================
+ */
+// 요소노드 생성
+var page = document.createElement('div'); // <div>
+
+// 생성한 요소에 속성 추가
+attr(page, 'id', 'page');
+attr(page, 'class', 'page');
+attr(page, 'title', '페이지 요소');
+attr(page, 'data-ui-element', true);
+
+console.log(attr(page, 'id'));
+console.log(attr(page, 'class'));
+console.log(attr(page, 'title'));
+console.log(attr(page, 'data-ui-element'));
+
+// 텍스트노드 생성
+// 문서(DOM)에 삽입
+
+
+/**
+ * ======================================================================
  * 선택(Selecting) | 탐색(Traversing)
  * ======================================================================
  */
@@ -63,6 +86,23 @@ function children(parentEl, childrenSelector) {
 	}
 	return els.length === 0 ? null : els.length === 1 ? els[0] : els;
 }
+
+/**
+ * --------------------------------
+ * first()
+ * last()
+ * --------------------------------
+ */
+function first(parent, selector) {
+	return $(selector, parent)[0];
+}
+
+function last(parent, selector) {
+	var childs     = $(selector, parent),
+		childs_len = childs.length;
+	return childs[ childs_len - 1 ];
+}
+
 
 /**
  * ======================================================================
@@ -220,6 +260,7 @@ function checkUserAgent(device_name) {
 /**
  * ----------------------------------------------------------------------
  * 유효성 검사 헬퍼 함수
+ * is()
  * isNumber()
  * isString()
  * isBoolean()
@@ -228,6 +269,35 @@ function checkUserAgent(device_name) {
  * isObject()
  * ----------------------------------------------------------------------
  */
+
+function is(data, type) {
+
+	// validate(data, '첫번째 인자 값은 확인하고자 하는 데이터 값을 입력해주세요');
+
+	validate(isString(type), '두번째 인자 값은 문자열을 사용해야 합니다.');
+
+	switch( data.constructor ) {
+		case Number:
+			return type === 'number' || type === 'num';
+		break;
+		case String:
+			return type === 'string' || type === 'str';
+		break;
+		case Boolean:
+			return type === 'boolean' || type === 'boo';
+		break;
+		case Function:
+			return type === 'function' || type === 'fnc';
+		break;
+		case Array:
+			return type === 'array' || type === 'arr';
+		break;
+		case Object:
+			return type === 'object' || type === 'obj';
+	}
+
+}
+
 function isNumber(num) {
 	return typeof num === 'number';
 }
@@ -252,6 +322,31 @@ function isObject(obj) {
 	return typeof obj === 'object' && !obj.push;
 }
 
+
+// function isNumber(num) {
+// 	return is(num, 'number');
+// }
+
+// function isString(str) {
+// 	return is(str, 'string');
+// }
+
+// function isBoolean(boo) {
+// 	return is(boo, 'boolean');
+// }
+
+// function isFunction(fnc) {
+// 	return is(fnc, 'function');
+// }
+
+// function isArray(arr) {
+// 	return is(arr, 'array');
+// }
+
+// function isObject(obj) {
+// 	return is(obj, 'object');
+// }
+
 /**
  * ----------------------------------------------------------------------
  * isElement() 헬퍼 함수
@@ -274,9 +369,26 @@ function isNodeList(list) {
  * ----------------------------------------------------------------------
  */
 function validate(condition, error_msg) {
-	if(typeof condition !== 'undefined' && !condition) {
+	if(
+		condition === undefined &&
+		condition === null &&
+		condition !== 0 &&
+		condition !== ''
+	) {
 		throw new TypeError(error_msg);
 	}
 }
 
-
+/**
+ * --------------------------------
+ * override()
+ * 객체 A에 객체 B의 멤버를 복제
+ * 동일한 멤버가 있을 경우 덮어씀(Override)
+ * --------------------------------
+ */
+function override() {
+	for( var key in obj2 ) {
+		obj1[key] = obj2[key];
+	}
+	return obj1;
+}
