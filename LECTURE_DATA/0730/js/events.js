15f99b10b8dc51f5f369784276738f28c82c3504
Jee Hoon <yamoo9@naver.com>
2015-07-30 17:57:55 +0900

    [코드 정리] 주석 추가

--- LECTURE_DATA/0730/js/events.js
+++ LECTURE_DATA/0730/js/events.js
@@ -7,6 +7,8 @@
 	// UI 초기화 함수
 	function init() {
 
+		// detectDeviceWidthInit() 함수가 실행되면서
+		// 클로저 함수를 반환 detectDeviceWidth 변수에 참조
 		var detectDeviceWidth = detectDeviceWidthInit({
 			// 'mobile': 480,
 			'tablet': 1200,
@@ -18,23 +20,59 @@
 		});
 
 		// 로딩이 되었을 때, 실행
+		// detectDeviceWidth 는 클로저 함수
 		detectDeviceWidth();
 
 		// 창 크기를 조정할 때, 실행
+		// detectDeviceWidth 는 클로저 함수
 		global.onresize = detectDeviceWidth;
 	}
 
+
+
+
+
+
+
+
+
+
+
+
+
+	/**
+	 * 기기(장치)의 폭을 감지하는 함수(상위 스코프)
+	 * @param  {Object} options 사용자정의 객체
+	 * @return {Function}       클로저 함수
+	 */
 	function detectDeviceWidthInit(options) {
 
+		// ------------------------------------------------------------------------------------
 		// 유효성 검사
 		y9.validate( y9.isObject(options), '객체를 전달해야 합니다.' );
 
+		// ------------------------------------------------------------------------------------
 		// 지역 함수
+		// 상위 스코프 함수
+		// 글로벌 스코프에서는 접근 불가
+		// 하지만 클로저 함수에서는 접근 가능
 		function getDeviceWidth() {
 			return global.innerWidth || y9.$('body').clientWidth;
 		}
 
+		function removeClass(id) {
+			y9.hasClass(config.targetEl, config.deviceId[id]) && y9.removeClass(config.targetEl, config.deviceId[id]);
+		}
+
+		function addClass(id) {
+			y9.hasClass(config.targetEl, config.deviceId[id]) || y9.addClass(config.targetEl, config.deviceId[id]);
+		}
+
+		// ------------------------------------------------------------------------------------
 		// 지역 변수
+		// 상위 스코프 함수
+		// 글로벌 스코프에서는 접근 불가
+		// 하지만 클로저 함수에서는 접근 가능
 		// 기본 옵션
 		var defaults = {
 				'mobile': 800,
@@ -48,22 +86,22 @@
 			}, config;
 
 		// 함수 실행 시, 사용자 정의 옵션과 기본 옵션 오버라이딩
-		config = options ?
-			y9.override(defaults, options) :
-			defaults;
+		// 사용자의 인터렉션과 상관 없이 1회만 실행
+		config = options ? y9.override(defaults, options) : defaults;
 
-		function removeClass(id) {
-			y9.hasClass(config.targetEl, config.deviceId[id]) && y9.removeClass(config.targetEl, config.deviceId[id]);
-		}
 
-		function addClass(id) {
-			y9.hasClass(config.targetEl, config.deviceId[id]) || y9.addClass(config.targetEl, config.deviceId[id]);
-		}
 
-		// Javascript Closure Function
+		// ------------------------------------------------------------------------------------
+		// 클로저 함수(반환 함수)
+		// 초기화 이후, 창 크기를 조정할 때 마다 실행되는 함수
 		return function() {
+
+			// 상위 스코프의 getDeviceWidth() 함수 실행
+			// 클로저이기 때문에 가능
 			var detectW = getDeviceWidth();
 
+			// 상위 스코프의 config 변수 참조
+			// 클로저이기 때문에 가능
 			if ( detectW < config.mobile ) {
 				// 제거
 				y9.each(['tablet', 'desktop'], removeClass);
@@ -71,16 +109,13 @@
 				addClass('mobile');
 			}
 			else if( detectW >= config.mobile && detectW < config.tablet ) {
-				// 제거
 				y9.each(['mobile', 'desktop'], removeClass);
-				// 추가
 				addClass('tablet');
 			} else {
-				// 제거
-				y9.each(['mobile', 'tablet'], removeClass);
-				// 추가
+				y9.each(['mobile', 'tablet'], removeClass);가
 				addClass('desktop');
 			}
+
 		}
 	}
 
