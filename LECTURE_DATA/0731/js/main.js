54745adba23a249282cad6b7d0d8396476b6961e
Jee Hoon <yamoo9@naver.com>
2015-07-31 10:40:43 +0900

    이벤트 진행 코드 v1

---
+++ LECTURE_DATA/0731/js/main.js
@@ -0,0 +1,55 @@
+/*! main.js © yamoo9.net, 2015 */
+
+/**
+ * --------------------------------
+ * JS Legacy Event
+ *
+ * Interface Events
+ * load
+ * resize
+ * scroll
+ *
+ * --------------------------------
+ */
+
+// 즉시 실행하는 함수 (스코프 함수)
+// 캡슐화 (다른 코드와 충돌 방지)
+(function(global, $){
+	'use strict';
+
+	// global 객체 이벤트 연결
+	global.onload = uiInit;
+
+	/**
+	 * --------------------------------
+	 * UI 초기화 시에 수행되는 함수
+	 */
+	function uiInit() {
+
+		// 동적으로 콘텐츠를 추가하는 함수 실행
+		dynamicAddContents();
+
+		// 이벤트 연결
+		this.onscroll = checkEventScroll;
+	}
+
+	/**
+	 * --------------------------------
+	 * 이벤트 핸들러 (함수 모음)
+	 */
+	function checkEventScroll() {
+		console.log(this === global);
+		console.log(this === window);
+	}
+
+	function dynamicAddContents() {
+		// <body> 요소에 동적으로 콘텐츠를 만들어 붙인다.
+
+		// <div id="app"> 요소 생성
+		var app = $.createEl('div');
+		$.attr(app, 'id', 'app');
+		console.log(app);
+	}
+
+
+})(window, window.yamoo9);
