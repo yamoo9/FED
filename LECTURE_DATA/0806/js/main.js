c8fcad3ee4337a016dd0b78e2ff71f716d3f4ff4
Jee Hoon <yamoo9@naver.com>
2015-08-06 14:34:15 +0900

    템플릿 처리

--- LECTURE_DATA/0806/js/main.js
+++ LECTURE_DATA/0806/js/main.js
@@ -5,38 +5,68 @@
 
 	function makeFriend(f_name, f_job, f_beverage) {
 		return {
-			'name'     : f_name;     // global.prompt('당신이 좋아하는 친구의 이름은?'),
-			'job'      : f_job;      // global.prompt('그 친구의 직업은?'),
-			'beverage' : f_beverage; // global.prompt('그 친구가 좋아하는 음료는?'),
+			'name'     : f_name,     // global.prompt('당신이 좋아하는 친구의 이름은?'),
+			'job'      : f_job,      // global.prompt('그 친구의 직업은?'),
+			'beverage' : f_beverage, // global.prompt('그 친구가 좋아하는 음료는?'),
 		}
 	}
 
 	myFriendsList.push( makeFriend('희진', '놀이공원 매점원', '오곡라떼') );
 	myFriendsList.push( makeFriend('경민', '국정원 비밀요원', '식혜') );
+	myFriendsList.push( makeFriend('수현', '텔런트', '당근 주스') );
+	myFriendsList.push( makeFriend('소라', '가수', '홍초') );
+	myFriendsList.push( makeFriend('알버트 아인슈타인', '과학자', '우유') );
+	myFriendsList.push( makeFriend('슈바이처', '의사', '물') );
 
-	console.log(myFriendsList);
+	// 객체를 전달하면 해당 객체의 멤버를 확인하여 HTML 코드로 변경합니다.
+	function assignTemplate(obj) {
+		return '<li>'+obj.name+'</li><li>'+obj.job+'</li><li>'+obj.beverage+'</li>';
+	}
 
-	return;
+
+	function makeTempateCode(arr) {
+		for( var i=0, mergeHTMLCode='', l=arr.length; i<l; i++) {
+			var makedTempateHTMLCode = assignTemplate(arr[i]);
+			mergeHTMLCode += makedTempateHTMLCode;
+		}
+		return '<ul>' + mergeHTMLCode +'</ul>';
+	}
+
+	var finished_process = makeTempateCode(myFriendsList);
+
+	// console.log(makedTempateHTMLCode);
+
+	// for( var l=myFriendsList.length, i=0; l>i; i++ ) {
+	// 	var name     = myFriendsList[i].name,
+	// 		job      = myFriendsList[i].job,
+	// 		beverage = myFriendsList[i].beverage;
+
+	// 	// console.log(name, '\n'+job, '\n'+beverage);
+	// }
+
+	// return;
 
 	// DOM Element Selecting
 	// 문서에서 #app 요소를 찾아와 app 변수에 참조한다.
 	var app = document.querySelector('#app');
 
-	// SET
-	// app 요소에 텍스트 내용을 추가한다.
-	$.text(app, 'Hi! My Name is '+ myFreind.name +'. My Job is '+ myFreind.job +'!!');
+	// // SET
+	// // app 요소에 텍스트 내용을 추가한다.
+	// $.text(app, 'Hi! My Name is '+ myFreind.name +'. My Job is '+ myFreind.job +'!!');
 
-	// GET
-	// 변수 origin_html_code에 app 요소의 html 코드를 가져와 참조한다.
-	var origin_html_code = $.html(app);
+	// // GET
+	// // 변수 origin_html_code에 app 요소의 html 코드를 가져와 참조한다.
+	// var origin_html_code = $.html(app);
 
-	// SET
-	// app 요소에 origin_html_code 문자열 뒤에 새로운 HTML 코드를 붙여 추가한다.
-	$.html(app, origin_html_code + '<p>I like drink a '+ myFreind.beverage +'!</p>');
+	// // SET
+	// // app 요소에 origin_html_code 문자열 뒤에 새로운 HTML 코드를 붙여 추가한다.
+	// $.html(app, origin_html_code + '<p>I like drink a '+ myFreind.beverage +'!</p>');
 
-	// GET
-	// app 요소에 설정된 text 값, html 값을 각각 출력한다.
-	console.log( '[[TEXT]] ⇒ ', $.text(app) );
-	console.log( '[[HTML]] ⇒ ', $.html(app) );
+	// // GET
+	// // app 요소에 설정된 text 값, html 값을 각각 출력한다.
+	// console.log( '[[TEXT]] ⇒ ', $.text(app) );
+	// console.log( '[[HTML]] ⇒ ', $.html(app) );
+
+	$.html(app, finished_process);
 
 })(window, window.dom);
