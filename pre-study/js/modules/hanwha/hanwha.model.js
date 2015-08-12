782242e8b107492754fa6b171d10f4f1fabc3fca
Jee Hoon <yamoo9@naver.com>
2015-08-12 13:29:11 +0900

    주석 추가

--- pre-study/js/modules/hanwha/hanwha.model.js
+++ pre-study/js/modules/hanwha/hanwha.model.js
@@ -1,15 +1,19 @@
 (function(global){
 	'use strict';
 
+	// global.hanwha 객체가 없을 경우, 정의
 	if (!global.hanwha) {
 		Object.defineProperty(global, 'hanwha', { 'value': {} });
 	}
 
+	// global.hanwha.model 객체 정의
 	Object.defineProperty(hanwha, 'model', {
 		'value': (function() {
-			var that = this;
+
+			// 감춰진 멤버
 			var _database = {};
 
+			// 반환된 객체는 클로저로 상위 스코프의 지역 변수를 참조할 수 있음.
 			return {
 				'DB': _database,
 				'createModel': function(id, value) {
@@ -43,7 +47,7 @@
 				'get': function(id) {
 					return this.DB[id];
 				}
-			}
+			};
 
 		})()
 	});
