b5009c77346e14edc7d3fcdcb53d06262ca215d3
Jee Hoon <yamoo9@naver.com>
2015-08-13 16:35:39 +0900

    .on() 구문

--- LECTURE_DATA/0813/js/ui.js
+++ LECTURE_DATA/0813/js/ui.js
@@ -1,13 +1,44 @@
 (function(global, $, undefined){
 	'use strict';
 
+	/**
+	 * --------------------------------
+	 * UI 초기 실행
+	 * --------------------------------
+	 */
+
 	// UI 적용 대상 참조
-	var $accordion = $('.ui-accordion');
+	var $accordion          = $('.ui-accordion'),
+		$accordion_handles  = $accordion.find('[class*="-handle"]'),
+		$accordion_contents = $accordion.find('[class*="-content"]'),
+		random              = Math.floor( Math.random() *  $accordion_contents.length );
 
-	// UI 초기 실행
-	var $accordion_contents = $accordion.find('[class*="-content"]').hide();
+	// 아코디언 위젯의 모든 콘텐츠를 숨김
+	$accordion_contents
+		.hide()
+		// 아코디언 위젯 중 일부 콘텐츠는 보임(랜덤 설정)
+		.eq( random || 0 ).show();
 
-	// console.log($accordion_contents);
+	/**
+	 * --------------------------------
+	 * Event 핸들링
+	 * --------------------------------
+	 */
+	console.log($accordion_handles);
+	$accordion_handles.on('click', function() {
+		// jQuery의 .on() 함수 내 전달되는 함수 안에서의 this는
+		// DOM ElementNode 입니다. 고로... jQuery 인스턴스의
+		// 능력은 사용할 수 없습니다.
+		console.log(!!this.addClass); // false
+		return false;
+	});
+
+
+	/**
+	 * --------------------------------
+	 * Event 핸들러
+	 * --------------------------------
+	 */
 
 
 })(window, window.jQuery);
