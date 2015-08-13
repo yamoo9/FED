9f67a70a49057b256d62b96d921ffa1e266f416d
Jee Hoon <yamoo9@naver.com>
2015-08-13 17:30:05 +0900

    이벤트 연결 및 핸들러 분리 또는 클릭 시뮬레이션

--- LECTURE_DATA/0813/js/ui.js
+++ LECTURE_DATA/0813/js/ui.js
@@ -14,54 +14,56 @@
 		random              = Math.floor( Math.random() *  $accordion_contents.length );
 
 	// 아코디언 위젯의 모든 콘텐츠를 숨김
-	$accordion_contents
-		.hide()
-		// 아코디언 위젯 중 일부 콘텐츠는 보임(랜덤 설정)
-		.eq( random || 0 ).show();
+	$accordion_contents.hide();
+
 
 	/**
 	 * --------------------------------
-	 * Event 핸들링
+	 * Event 바인딩
 	 * --------------------------------
 	 */
 	// console.log($accordion_handles);
-	$accordion_handles.on('click', function(e) {
-		// jQuery의 .on() 함수 내 전달되는 함수 안에서의 this는
-		// DOM ElementNode 입니다. 고로... jQuery 인스턴스의
-		// 능력은 사용할 수 없습니다.
-		// console.log(!!this.addClass); // false
-		// console.log(this.nextElementSibling);
+	$accordion_handles.on('click', toggleContent);
 
-		// this.nextElementSibling.style.display = 'block';
-
-		var $this   = $(this),
-			$nextEl = $this.next();
-
-		if ( $this.hasClass('on') ) {
-			$this.removeclass('on');
-		} else {
-			$this.addClass('on');
-		}
-
-		$nextEl.toggle();
-
-		// $nextEl 보여지는 상태? 안 보여지는 상태?
-		// if ( $nextEl.css('display') === 'none' ) {
-		// if ( $nextEl.is(':hidden') ) {
-		// 	$nextEl.show();
-		// } else {
-		// 	$nextEl.hide();
-		// }
-
-		return false;
-	});
-
+	// 초기 실행
+	$accordion_handles.eq(random).click();
 
 	/**
 	 * --------------------------------
 	 * Event 핸들러
 	 * --------------------------------
 	 */
+	function toggleContent(e) {
+			// jQuery의 .on() 함수 내 전달되는 함수 안에서의 this는
+			// DOM ElementNode 입니다. 고로... jQuery 인스턴스의
+			// 능력은 사용할 수 없습니다.
+			// console.log(!!this.addClass); // false
+			// console.log(this.nextElementSibling);
 
+			// this.nextElementSibling.style.display = 'block';
+
+			var $this   = $(this),
+				$nextEl = $this.next();
+
+			$this.toggleClass('on');
+
+			// if ( $this.hasClass('on') ) {
+			// 	$this.removeClass('on');
+			// } else {
+			// 	$this.addClass('on');
+			// }
+
+			$nextEl.toggle();
+
+			// $nextEl 보여지는 상태? 안 보여지는 상태?
+			// if ( $nextEl.css('display') === 'none' ) {
+			// if ( $nextEl.is(':hidden') ) {
+			// 	$nextEl.show();
+			// } else {
+			// 	$nextEl.hide();
+			// }
+
+			return false;
+		}
 
 })(window, window.jQuery);
