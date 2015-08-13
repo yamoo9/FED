a044ddbafe833af5de80360544a1b3e282d90859
Jee Hoon <yamoo9@naver.com>
2015-08-13 16:06:15 +0900

    아코디언 스타일 추가

---
+++ LECTURE_DATA/0813/js/ui.js
@@ -0,0 +1,13 @@
+(function(global, $, undefined){
+	'use strict';
+
+	// UI 적용 대상 참조
+	var $accordion = $('.ui-accordion');
+
+	// UI 초기 실행
+	var $accordion_contents = $accordion.find('[class*="-content"]').hide();
+
+	// console.log($accordion_contents);
+
+
+})(window, window.jQuery);
