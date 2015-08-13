221a92f2a478ac5a18a6efa8166522a3a2fb5fd1
Jee Hoon <yamoo9@naver.com>
2015-08-13 17:01:24 +0900

    .is() 문 활용

--- LECTURE_DATA/0813/js/ui.js
+++ LECTURE_DATA/0813/js/ui.js
@@ -24,12 +24,26 @@
 	 * Event 핸들링
 	 * --------------------------------
 	 */
-	console.log($accordion_handles);
+	// console.log($accordion_handles);
 	$accordion_handles.on('click', function() {
 		// jQuery의 .on() 함수 내 전달되는 함수 안에서의 this는
 		// DOM ElementNode 입니다. 고로... jQuery 인스턴스의
 		// 능력은 사용할 수 없습니다.
-		console.log(!!this.addClass); // false
+		// console.log(!!this.addClass); // false
+		// console.log(this.nextElementSibling);
+
+		// this.nextElementSibling.style.display = 'block';
+
+		var $nextEl = $(this).next();
+
+		// $nextEl 보여지는 상태? 안 보여지는 상태?
+		// if ( $nextEl.css('display') === 'none' ) {
+		if ( $nextEl.is(':hidden') ) {
+			$nextEl.show();
+		} else {
+			$nextEl.hide();
+		}
+
 		return false;
 	});
 
