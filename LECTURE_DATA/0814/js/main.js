02320be1bbd2babcacee79835b06b9021080ebfa
Jee Hoon <yamoo9@naver.com>
2015-08-14 14:53:48 +0900

    .css() 메소드 완벽 분석

--- LECTURE_DATA/0814/js/main.js
+++ LECTURE_DATA/0814/js/main.js
@@ -18,6 +18,7 @@
 		app_padding_bottom = $app.css('padding-bottom'),
 		app_padding_top    = $app.css('padding-top');
 
+
 	// PX2EM
 	function px2em(value, base) {
 		base = base || 16;
@@ -31,15 +32,51 @@
 	}
 
 	// PX
-	console.log( 'app_padding_left: ', app_padding_left );
-	console.log( 'app_padding_right: ', app_padding_right );
-	console.log( 'app_padding_bottom: ', app_padding_bottom );
-	console.log( 'app_padding_top: ', app_padding_top );
+	// console.log( 'app_padding_left: ', app_padding_left );
+	// console.log( 'app_padding_right: ', app_padding_right );
+	// console.log( 'app_padding_bottom: ', app_padding_bottom );
+	// console.log( 'app_padding_top: ', app_padding_top );
 
 	// EM
-	console.log( 'app_padding_left: ', px2em(app_padding_left) );
-	console.log( 'app_padding_right: ', px2em(app_padding_right) );
-	console.log( 'app_padding_bottom: ', px2em(app_padding_bottom) );
-	console.log( 'app_padding_top: ', px2em(app_padding_top) );
+	// console.log( 'app_padding_left: ', px2em(app_padding_left) );
+	// console.log( 'app_padding_right: ', px2em(app_padding_right) );
+	// console.log( 'app_padding_bottom: ', px2em(app_padding_bottom) );
+	// console.log( 'app_padding_top: ', px2em(app_padding_top) );
+
+	// SET
+	// 속성을 개별적으로 적용한 사례
+	$app_child
+		.css('font-size', px2em('100px') )
+		.css('position', 'absolute')
+		.css('top', '10px')
+		.css('right', '30px');
+
+	// 속성 맵(Map: Object)을 적용한 사례
+	var app_css_map = {
+		'border-radius'    : '5px',
+		'background-image' : 'linear-gradient(-90deg, #FC4A54, #841C26)',
+	};
+
+	$app.css(app_css_map);
+
+
+	// GET Properties
+	var app_css_props       = $app.css(['position', 'z-index', 'opacity']),
+		app_child_css_props = $app_child.css(['float', 'margin-right', 'outline']);
+
+	// for ~ in 문으로 객체 속성 탐색
+	for(var prop in app_css_props) {
+		var value = app_css_props[prop];
+		console.log('prop: ' + prop + ', ' + 'value:', value);
+	}
+
+	for(var prop in app_child_css_props) {
+		var value = app_child_css_props[prop];
+		console.log('prop: ' + prop + ', ' + 'value:', value);
+	}
+
+	// 전역에 공개
+	global.px2em = px2em;
+	global.em2px = em2px;
 
 })(window, window.jQuery);
