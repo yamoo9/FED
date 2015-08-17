3fb75d445ea91269f8f12b5afb5e274b4589a4e5
Jee Hoon <yamoo9@naver.com>
2015-08-17 15:39:25 +0900

    스킵 콘텐츠 데모

--- LECTURE_DATA/0817/skip-nav/js/main.js
+++ LECTURE_DATA/0817/skip-nav/js/main.js
@@ -2,4 +2,22 @@
 (function(global, $, undefined){
 	'use strict';
 
+	/**
+	 * --------------------------------
+	 * 문서 객체 참조
+	 * --------------------------------
+	 */
+	var $skip_navigation = $('#skip-navigation');
+
+	// var $skip_nav_links = $skip_navigation.find('a').wrapAll('<ul>').wrap('<li>');
+
+	// $skip_nav_links.addClass('a11y-hidden focusable');
+
+	var $skip_links = $skip_navigation
+		.addClass('skip-content')
+		.find('a').addClass('a11y-hidden focusable');
+
+	console.log($skip_links); // [a, a, a]
+
+
 })(window, window.jQuery);
