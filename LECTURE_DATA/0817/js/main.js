59c1f308f6a56d91c08b818ae259bda69ebd904d
Jee Hoon <yamoo9@naver.com>
2015-08-17 10:29:41 +0900

    Native 코드 추가

--- LECTURE_DATA/0817/js/main.js
+++ LECTURE_DATA/0817/js/main.js
@@ -41,4 +41,37 @@
 	$box.before('<p class="before-box">Lorem ipsum dolor.</p>');
 	$box.after('<p class="after-box">dolor Lorem ipsum.</p>');
 
+	/**
+	 * --------------------------------
+	 * Native Javascript Methods
+	 * --------------------------------
+	 */
+	var head = document.querySelector('head'),
+		first_scrit = head.querySelectorAll('script').item(0);
+
+	// DOM API, insertBefore
+	// var d_link = document.createElement('link');
+	// d_link.setAttribute('rel', 'stylesheet');
+	// d_link.setAttribute('href', 'css/temp.css');
+	// first_scrit.parentNode.insertBefore(d_link, first_scrit);
+
+	// DOM API, insertAdjacentHTML()
+	// first_scrit.insertAdjacentHTML('afterend', '<link rel="stylesheet" href="css/temp.css" />');
+
+	function append(target, last) {
+		target.insertAdjacentHTML('beforeend', last);
+	}
+	function prepend(target, first) {
+		target.insertAdjacentHTML('afterbegin', first);
+	}
+	function insertBefore(target, prev) {
+		target.insertAdjacentHTML('beforebegin', prev);
+	}
+	function insertAfter(target, next) {
+		target.insertAdjacentHTML('afterend', next);
+	}
+
+	append(head, '<link rel="stylesheet" href="css/temp.css" />');
+
+
 })(window, window.jQuery);
