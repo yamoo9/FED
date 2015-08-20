4f275a480d8fea379bf596fbae39d9ed2971d9de
Jee Hoon <yamoo9@naver.com>
2015-08-20 10:02:36 +0900

    radioClass 확장 및 플러그인 기초 공부

--- LECTURE_DATA/0819/js/main.js
+++ LECTURE_DATA/0819/js/main.js
@@ -2,6 +2,10 @@
 (function(global, $, undefined){
 	'use strict';
 
+	// $('*').toArray().reverse().forEach(function(item, index){
+	// 	console.log(item, index);
+	// });
+
 	console.log( $.cache, $.fn.radioClass );
 
 })(window, window.jQuery);
--- LECTURE_DATA/Libs/jquery.radioClass.js
+++ LECTURE_DATA/Libs/jquery.radioClass.js
@@ -2,21 +2,37 @@
 ;(function(global, $, undefined){
 	'use strict';
 
-	// jQuery 인스턴스 메소드인 radioClass가 정의되어 있는지 확인 후,
-	// 없다면 정의
 	if ( !$.fn.radioClass ) {
-		$.fn.radioClass = function(class_name) {
 
-			if ( $.type(class_name) !== 'string' ) {
-				throw console.error('라디오클래스를 적용할 클래스 속성 문자열을 전달해야 합니다.')
+		$.fn.radioClass = function(user_argument) {
+
+			// 사용자 전달인자 유형 체크를 위한 변수 설정
+			// $.type() 유틸리티 메소드 활용
+			var _type = $.type(user_argument);
+
+			// 전달인자 유효성 검사
+			if ( _type !== 'string' && _type !== 'function' ) {
+				throw console.error('라디오클래스를 적용할 클래스 속성 문자열이거나, 함수를 전달해야 합니다.');
 			}
 
-			// 플러그인 코드 안에서 this는 jQuery 인스턴스 객체이다.
-			this.addClass(class_name).siblings('.'+class_name).removeClass(class_name);
+			// _type 값이 문자일 때
+			if(_type === 'string')
+			{
+				this.addClass(user_argument).siblings('.'+user_argument).removeClass(user_argument);
+				return this;
+			}
+			// _type 값이 함수일 때
+			else
+			{
+				return $.each(this, function(index, dom_el) {
+					var result_className = user_argument.call(dom_el, index, dom_el.getAttribute('class'));
+					this.addClass(result_className).siblings('.'+result_className).removeClass(result_className);
+					return this;
+				});
+			}
 
-			// jQuery 체이닝을 구현하기 위한 this 반환
-			return this;
-		}
+		}; // 플러그인 끝
+
 	}
 
 })(window, window.jQuery);
