9576cd880abc182d297cc5572c7c767d408b0a0b
Jee Hoon <yamoo9@naver.com>
2015-08-25 10:03:13 +0900

    jQuery.darkNight.js 코드

--- LECTURE_DATA/0825/AMD-RequireJS/js/main.js
+++ LECTURE_DATA/0825/AMD-RequireJS/js/main.js
@@ -1,1 +1,37 @@
 /*! main.js © yamoo9.net, 2015 */
+
+// 환경 설정
+require.config({
+
+	'baseUrl': 'js/libs',
+
+	'paths': {
+		// Libs
+		'modernizr'    : 'modernizr',
+		'detectizr'    : 'detectizr',
+		'jquery'       : 'jquery-2.1.4',
+		'jquery.utils' : 'jquery.utils',
+		// Plugins
+		'jquery.darkNight' : '../plugins/jquery.darkNight',
+	},
+
+	'shim': {
+		'detectizr': {
+			'deps': ['modernizr']
+		},
+		'jquery.utils': {
+			'deps': ['jquery']
+		},
+	}
+
+});
+
+// require() 호출
+require([
+	// 'detectizr',
+	// 'jquery.utils',
+	'jquery.darkNight'
+]);
+
+
+
---
+++ LECTURE_DATA/0825/AMD-RequireJS/js/plugins/jquery.darkNight.js
@@ -0,0 +1,15 @@
+// AMD Pattern
+// define() 함수
+// require() 함수
+
+define([
+	// 'jquery',
+	'jquery.utils'
+],
+function() {
+	'use strict';
+	// jQuery, jQuery.utils > fn
+
+
+
+});
