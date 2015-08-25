b024394dfcc1a270a1b56b3a10622716760ddb6e
Jee Hoon <yamoo9@naver.com>
2015-08-25 10:27:26 +0900

    jquery.darkNight.js 플러그인 코드 개발 전 단계

--- LECTURE_DATA/0825/AMD-RequireJS/js/plugins/jquery.darkNight.js
+++ LECTURE_DATA/0825/AMD-RequireJS/js/plugins/jquery.darkNight.js
@@ -3,13 +3,35 @@
 // require() 함수
 
 define([
-	// 'jquery',
 	'jquery.utils'
 ],
 function() {
 	'use strict';
-	// jQuery, jQuery.utils > fn
 
+	// darkNight 스타일 추가
+	$('<link>', {
+		'id'   : 'dark-night',
+		'rel'  : 'stylesheet',
+		'href' : 'js/plugins/jquery.darkNight.css'
+	})
+	.insertAfter('link:eq(0)');
 
+	// Letter 초기 옵션
+	var letters = {
+		'first' : 'DARK',
+		'last'  : 'NIGHT',
+	};
+
+	// darkNight 템플릿
+	var dark_night_tempate = [
+		'<div id="box">',
+			'<p id="flashlight">',
+				'<span id="flash">'+ letters.first +'</span>',
+				'<span id="light">'+ letters.last +'</span>',
+			'</p>',
+		'</div>'].join('');
+
+	// darkNight 마크업 추가
+	$(dark_night_tempate).prependTo('body');
 
 });
